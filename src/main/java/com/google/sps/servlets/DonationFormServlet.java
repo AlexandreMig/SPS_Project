9cd2903876e66290adcfb6755308ca08e8e538e3
Keianna <nagle.keianna@gmail.com>
2021-03-25 20:50:03 +0000

    Create Donation Form Servlet

---
+++ src/main/java/com/google/sps/servlets/DonationFormServlet.java
@@ -0,0 +1,29 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/donation-form")
+public class DonationFormServlet extends HttpServlet {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    // Donation form info
+      String name = request.getParameter("name");
+      String location = request.getParameter("location");
+      String item = request.getParameter("item");
+      String number = request.getParameter("number");
+
+    // Print form value to server logs
+      System.out.println(name + "'s location is" + location + ". They would like to donate " + item + " and their phone number is " + number +".");
+
+    }
+}
