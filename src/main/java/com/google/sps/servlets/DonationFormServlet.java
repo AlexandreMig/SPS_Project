4ce1968a25e3cf05b94d507a42e4e557fbcf8b9c
Keianna <nagle.keianna@gmail.com>
2021-03-26 23:05:31 +0000

    Remove empty comments

--- src/main/java/com/google/sps/servlets/DonationFormServlet.java
+++ src/main/java/com/google/sps/servlets/DonationFormServlet.java
@@ -8,9 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/donation-form")
 public class DonationFormServlet extends HttpServlet {
-    /**
-     *
-     */
+
     private static final long serialVersionUID = 1L;
 
     @Override
